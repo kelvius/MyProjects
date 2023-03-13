1c57591f66912f02e84b7942f36e972892337cc2
kelvius <valeriojohnkelvin@gmail.com>
2023-03-13 16:39:36 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*******w******** 
+Name: John Kelvin A. Valerio
+Date: 03/13/23
+Description: Project 
+****************/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Drip Book</title>
+</head>
+
+<body>
+</body>
+</html>
