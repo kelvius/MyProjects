1bdb7fe028d78b40ae8bfd65f0c3eb31f6bb25e8
kelvius <valeriojohnkelvin@gmail.com>
2023-03-15 16:14:45 -0500

    Created initial Create post

---
+++ create.php
@@ -0,0 +1,61 @@
+<?php
+
+/*******w******** 
+Name: John Kelvin A. Valerio
+Date: 03/13/23
+Description: Project
+****************/
+
+
+require('connect.php');
+require('authenticate.php');
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Edit this Post!</title>
+</head>
+
+<body>
+    <!-- Remember that alternative syntax is good and html inside php is bad -->
+    <div id="wrapper">
+        <div id="header">
+            <h1><a href="index.php">Kelvin's Blog - New Blog Post</a></h1>
+        </div>
+        <ul id="menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="create.php" class="active">New Post</a></li>
+        </ul>
+        <div id="all_blogs">
+            <form action="post.php" method="post">
+                <fieldset>
+                    <legend>Edit Drip Post</legend>
+                    <p>
+                        <label for="title">Title</label>
+                        <input name="title" id="title" >
+                    </p>
+                    <p>
+                        <label for="content">Content</label>
+                        <textarea name="content" id="content"></textarea>
+                    </p>
+                    <p>
+                        <input type="hidden" name="id">
+                        <input type="submit" name="create" value="Create" >
+                    </p>
+                </fieldset>
+            </form>
+        </div>
+        <div id="footer">
+            Copywrong 2023 - No Rights Reserved
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ post.php
@@ -0,0 +1,103 @@
+<?php
+
+/*******w******** 
+Name: John Kelvin A. Valerio
+Date: 03/13/23
+Description: Project
+****************/
+
+require('connect.php');
+
+if (
+    $_POST && !empty($_POST['title']) && strlen($_POST['title']) >= 1 &&
+    strlen($_POST['title']) <= 100 && !empty($_POST['content']) && strlen($_POST['content']) >= 1
+) {
+    if (isset($_POST['create'])) {
+        //  Sanitize user input to escape HTML entities and filter out dangerous characters.
+        $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRIPPED);
+        $content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_STRIPPED);
+
+        //  Build the parameterized SQL query and bind to the above sanitized values.
+        $query = "INSERT INTO blogs (title, content) VALUES (:title, :content)";
+        $statement = $db->prepare($query);
+
+        //  Bind values to the parameters
+        $statement->bindValue(":title", $title);
+        $statement->bindValue(":content", $content);
+
+        //  Execute the INSERT.
+        //  execute() will check for possible SQL injection and remove if necessary
+        if ($statement->execute()) {
+            header("Location: index.php");
+            exit;
+        }
+    } else if (isset($_POST['update'])) {
+        //  Sanitize user input to escape HTML entities and filter out dangerous characters.
+        $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRIPPED);
+        $content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_STRIPPED);
+        $id = filter_input(INPUT_POST, 'post_id', FILTER_VALIDATE_INT);
+
+        //  Build the parameterized SQL query and bind to the above sanitized values.
+        //$query = "UPDATE blogs SET title=$title, content=$content WHERE id=$id";
+        $query = "UPDATE blogs SET title = :title, content = :content WHERE post_id = :post_id";
+        $statement = $db->prepare($query);
+
+        //  Bind values to the parameters
+        $statement->bindValue(":title", $title);
+        $statement->bindValue(":content", $content);
+        $statement->bindValue(":post_d", $post_id);
+
+        //  Execute the UPDATE.
+        //  execute() will check for possible SQL injection and remove if necessary
+        if ($statement->execute()) {
+            header("Location: index.php");
+            exit;
+        }
+
+    } else if (isset($_POST['delete'])) {
+        //  Sanitize user input to escape HTML entities and filter out dangerous characters.
+        $id = filter_input(INPUT_POST, 'post_id', FILTER_VALIDATE_INT);
+        
+        //  Build the parameterized SQL query and bind to the above sanitized values.";
+        $query = "DELETE FROM blogs WHERE post_id = :post_id";
+        $statement = $db->prepare($query);
+
+        //  Bind values to the parameters
+        $statement->bindParam(":;post_id", $post_id);
+
+        //  Execute the DELETE.
+        //  execute() will check for possible SQL injection and remove if necessary
+        if ($statement->execute()) {
+            header("Location: index.php");
+            exit;
+        }
+
+    }
+} else {
+    $errorMessage = "The tweet message or title is empty";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>My Drip Post!</title>
+</head>
+
+<body>
+    <!-- Remember that alternative syntax is good and html inside php is bad -->
+    <?php if (!empty($errorMessage)): ?>
+        <h1>
+            <?= $errorMessage ?>
+        </h1>
+    <?php endif ?>
+</body>
+
+</html>
